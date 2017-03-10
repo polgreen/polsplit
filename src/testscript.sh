177772095ec8160ed7dd648ec133e1cd0855cac0
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-03-10 18:51:57 +0000

    update test script

--- src/testscript.sh
+++ src/testscript.sh
@@ -1,7 +1,7 @@
 
 echo "Bash version ${BASH_VERSION}..."
 
-for i in `seq 5 5 95`;
+for i in `seq 5 5 75`;
 do
    >model_param.h
 	echo "#ifndef MODELPARAM_H">>model_param.h
@@ -12,12 +12,22 @@ do
 	echo "#endif">>model_param.h
 	./compile
 	
-	for trace_length in `seq 10 10 200`;
+	for trace_length in `seq 10 10 100`;
 	do
-		for number_traces in `seq 10 50 1000`;
+		for number_traces in `seq 10 10 100`;
 		do
-			./baever --MDP 2 --traces $number_traces --tracelength $trace_length	
+			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 10
+			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 10 --random_strategy	
 		done
 	done	
+	
+	for trace_length in 'seq 120 20 200'
+	do
+		for number_traces in 'seq 150 50 1000'
+		do
+			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 1
+			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 1	--random_strategy
+		done
+	done		
 done   
 
