de1bde8add871fcde9de26f1b38b8d3b5be50079
polgreen <elizabeth.polgreen@cs.ox.ac.uk>
2017-03-14 11:56:37 +0000

    test script param range

--- src/testscript.sh
+++ src/testscript.sh
@@ -1,7 +1,7 @@
 
 echo "Bash version ${BASH_VERSION}..."
 
-for i in `seq 5 5 75`;
+for i in `seq 15 5 75`;
 do
    >model_param.h
 	echo "#ifndef MODELPARAM_H">>model_param.h
@@ -12,21 +12,13 @@ do
 	echo "#endif">>model_param.h
 	./compile
 	
-	for trace_length in `seq 10 10 100`;
+	for trace_length in `seq 10 30 100`;
 	do
-		for number_traces in `seq 10 10 100`;
+		for number_traces in `seq 10 100 210`;
 		do
-			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 10
-			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 10 --random_strategy	
-		done
-	done	
-	
-	for trace_length in 'seq 120 20 200'
-	do
-		for number_traces in 'seq 150 50 1000'
-		do
-			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 1
-			./baever --MDP 2 --traces $number_traces --tracelength $trace_length --batch 1	--random_strategy
+			./baever --MDP 3 --traces $number_traces --tracelength $trace_length --batch 10 --explicit_strategy
+			./baever --MDP 3 --traces $number_traces --tracelength $trace_length --batch 10 --random_strategy
+			./baever --MDP 3 --traces $number_traces --tracelength $trace_length --batch 10 --first_strategy	
 		done
 	done		
 done   
